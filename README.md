792234485086163b77a7298a6b5794b7d4ef38b6
Bemon <tristan2107b@gmail.com>
2023-07-27 19:52:09 +1000

    Made readme and a plethora of changes

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # README-Generator-Challenge7-Node.JS
+  ![GitHub](https://img.shields.io/badge/license-mit-blue)
+
+  ## Description
+  A high quality README generator allowing users to streamline the process of making a README by giving the individual easy to answer questions from which a detailed README is generated allowing for more time to focus on code itself
+
+  ## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#Questions)
+  
+  ## Installation
+  npm install inquirer@8.2.4 is needed to be installed to get node_modules
+  
+  ## Usage
+  run "node index.js" in the intergrated terminal, this will make a folder named "Generated README" if one does not already exist and then the generated README will be put inside
+  
+  ## Contributing
+  refer to "Questions" for contact information to contribute
+  
+  ## Tests
+  npm run test
+  
+  ## License
+  MIT
+
+  ## Questions
+For any questions, please feel free to reach out through the following channels:
+- Email: tristan2107b@gmail.com
+- Github: [Bemonn](https://github.com/Bemonn)
---
+++ index.js
@@ -0,0 +1,136 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const questions = [
+  {
+    type: 'input',
+    name: 'projectTitle',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Give a description of your project',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'How do you install your project?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use your project?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'How can others contribute to your project?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'How to test your project?',
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have?",
+    choices: [
+      "No license",
+      "Apache License 2.0",
+      "GNU v3",
+      "MIT",
+      "BSD 2-Clause",
+      "BSD-3-Clause",
+      "Boost",
+      "Creative Commons",
+      "Eclipse",
+      "GNU GPL v3",
+      "Mozilla",
+      "The Unlicense",
+    ],
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'Enter your Github username:',
+  },
+];
+
+function renderLicenseBadge(license) {
+    let licenseFormatted = license.toLowerCase().split(' ').join('%20');
+    if (licenseFormatted === 'no%20license') {
+      return '';
+    } else {
+      return `![GitHub](https://img.shields.io/badge/license-${licenseFormatted}-blue)`;
+    }
+  }
+
+  function writeToFile(fileName, data) {
+    const dir = './Generated README';
+
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
+
+    fileName = `${dir}/${fileName}`;
+
+    fs.writeFile(fileName, data, (err) => {
+      if (err) {
+        return console.log(err);
+      }
+  
+      console.log("Successfully wrote: " + fileName);
+    });
+}
+  
+  function init() {
+    inquirer.prompt(questions)
+      .then((answers) => {
+        let output = 
+        `# ${answers.projectTitle}
+  ${renderLicenseBadge(answers.license)}
+
+  ## Description
+  ${answers.description}
+
+  ## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#Questions)
+  
+  ## Installation
+  ${answers.installation}
+  
+  ## Usage
+  ${answers.usage}
+  
+  ## Contributing
+  ${answers.contributing}
+  
+  ## Tests
+  ${answers.tests}
+  
+  ## License
+  ${answers.license}
+
+  ## Questions
+For any questions, please feel free to reach out through the following channels:
+- Email: ${answers.email}
+- Github: [${answers.username}](https://github.com/${answers.username})
+        `;
+  
+        writeToFile("README.md", output);
+      });
+  }
+  
+  init();
